25b2850c3456a8a3deb5650d25b1b84ae6d91567
Thomas <tfreeman1@mail.usf.edu>
2015-10-11 16:43:11 -0400

    enqueue working correctly including doubling array size and coping elements if needed. O(1) average time

--- Project 2 - Grocery Store Simulation/DynamicQueue.h
+++ Project 2 - Grocery Store Simulation/DynamicQueue.h
@@ -1,6 +1,10 @@
 #ifndef DYNAMIC_QUEUE_H
 #define DYNAMIC_QUEUE_H
 
+#include <iostream>
+
+using namespace std;
+
 
 template <class QueueElement>
 class dynamic_queue {
@@ -8,7 +12,15 @@ class dynamic_queue {
 public:
 
 	//default constructor
-	dynamic_queue() {}
+	dynamic_queue() {
+		cout << "constructor";
+		itail = 0;
+		ihead = 0;
+		initial_capacity = 3;	//temp
+		array_capacity = 3;
+		myArray = new QueueElement [initial_capacity];
+		memset(myArray, 0, initial_capacity);
+	}
 
 	//copy constructor
 	dynamic_queue(const dynamic_queue&) {}
@@ -37,29 +49,118 @@ public:
 		}
 	}
 
+	bool isFull() {
+	
+		if (getSize() == array_capacity) return true;
+		else return false;	
+	}
+
+	//creates a new array with double the capacity of the current array and copies each element into the new array
+	void increaseSize() {
+	
+		QueueElement * newArray;
+		newArray = new QueueElement [getCapacity() * 2];
+
+		//copy each element of the old array
+		for (int i = 0; i < getSize(); i++) {
+
+			newArray[i] = myArray[i];
+		}
+
+		//delete any dynamically created elements of the old array
+		/*for (int i = 0; i < getSize(); i++) {
+
+			delete myArray[i];
+		}*/
+
+		//point myArray variable to the newly created array
+		myArray = newArray;	
+
+		array_capacity *= 2;
+
+		
+	};
+
+	void decreaseSize() {};
+
 
 	//returns the capacity of the dynamic array as an int
-	int getCapacity() {
+	int getCapacity() {		
 
-		//need to find a way to return the capacity of the array, not the number of elements in it
+		return array_capacity;
 	}
 
-	void display(ostream & out) const {
+	void display(ostream &out) const {
+
+		cout << "*****" << endl;
+		int i = 0;
+		for (i = 0; i < getSize(); i++) {
+		
+			cout << myArray[i] << endl;
+		}
+
+		if (i == 0) {
+		
+			cout << "Array is Empty" << endl;
+		
+		}	
+	}
+
+
+	void enqueue(const QueueElement &value) {
+
+		if (isFull() != true) {
+
+			itail++;
+			myArray[itail - 1 % getCapacity()] = value;
+			cout << itail - 1 % getCapacity();
+			cout << myArray[itail - 1 % getCapacity()] << endl;
+			array_size++;
+		}
+		else {
+		
+			increaseSize();
+			enqueue(value);		
+		}
+	}
+
+
+	void dequeue() {
 	
+		--ihead;
+
+		if (getSize() <= (.25 * initial_capacity)) {
+		
+			decreaseSize();
+		
+		}	
+	}
+
+
+	bool swap(dynamic_queue &a, dynamic_queue &b) {
+	
+		QueueElement *temp = NULL;
+
+		if (temp) {
+
+			temp = a;
+			a = b;
+			b = temp;
+
+			return true;
+		}
+		else {
+
+			return false;
+		}
 	
 	}
 
 
-	void enqueue(const QueueElement & value) {}
-
-
-	void dequeue() {}
-
-
-	bool swap(dynamic_queue&, dynamic_queue&) {}
-
-
-	void clear() {}
+	void clear() {
+	
+		
+	}
 	
 	const dynamic_queue& operator=(const dynamic_queue&) {}
 
@@ -68,8 +169,8 @@ public:
 
 private:
 	/***** Data Members *****/
-	int ihead,
-		itail;
+	int ihead;
+	int	itail;
 	int initial_capacity; // initial capacity of array
 	int array_capacity; // current capacity of the array
 	int array_size; // current size of queue stored in array
--- Project 2 - Grocery Store Simulation/main.cpp
+++ Project 2 - Grocery Store Simulation/main.cpp
@@ -1,14 +1,33 @@
 #ifndef MAIN_CPP
 #define MAIN_CPP
 
+///////////////////////////////////
+//	Thomas Freeman
+//	Data Structures Project 2 - Grocery Store Simulation
+///////////////////////////////////
+
+#include "DynamicQueue.h"
+
+#include <iostream>
+
+;using namespace std;
+
 int main() {
 
+	dynamic_queue<int> queue1;
+
+	queue1.enqueue(1);
+	queue1.enqueue(2);
+	queue1.enqueue(3);
+	queue1.enqueue(4);
+
+	queue1.display(cout);
+
+	cin.ignore();
 
 
 	return 0;
 }
-
-
 
 
 
